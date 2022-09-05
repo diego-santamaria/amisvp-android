777316d1e88f1ba4cb16961c92c8ed6abf78a850
Diego Santamaria <21200741+devsantasot@users.noreply.github.com>
2022-09-05 16:02:55 -0500

    HU003	Grabación de la evaluación (Done)

---
+++ app/src/main/java/com/example/amisvp/helper/CloudHelper.java
@@ -0,0 +1,13 @@
+package com.example.amisvp.helper;
+//import com.microsoft.azure.storage.*;
+//import com.microsoft.azure.storage.blob.*;
+
+public class CloudHelper {
+
+    public static final String storageConnectionString =
+            "DefaultEndpointsProtocol=https;" +
+                    "AccountName=thesisprojectstoacc;" +
+                    "AccountKey=cHhwPJI1YFDwm4IW8VrYSlrS7/eBNAl0yjhCzJItjRbwOKaaTRE+3CGLbQ8trWUYVt2JmcmgtxVM+ASteqlIAw==;EndpointSuffix=core.windows.net";
+
+
+}
